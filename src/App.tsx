961d08211e907f778abe3af2c76e3ef8cd031756
William Tristão de Paula <william@grupowi.com.br>
2021-01-31 18:32:29 -0300

    ::Implementation:: Status bar color changed

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,5 @@
 import React, { FC } from 'react';
+import { StatusBar } from 'react-native';
 import { Provider } from 'react-redux';
 import Navigator from './navigator';
 import store from './store/ducks/tv_show/store';
@@ -6,6 +7,7 @@ import store from './store/ducks/tv_show/store';
 const App: FC = () => {
   return (
     <Provider store={store} >
+      <StatusBar barStyle='light-content' backgroundColor='#000' />
       <Navigator />
     </Provider>
   );
